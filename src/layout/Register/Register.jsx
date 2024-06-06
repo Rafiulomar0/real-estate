c9b21a89c0475c4bbb8b8b5088e333070d32ed83
RafiulOmar <rafiulomar2022@gmail.com>
2024-06-07 01:34:53 +0600

    Register page updated

--- src/layout/Register/Register.jsx
+++ src/layout/Register/Register.jsx
@@ -1,40 +1,83 @@
 import { Link } from "react-router-dom";
 import { FaUser, FaLink } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { RiLockPasswordFill, RiEyeCloseFill } from "react-icons/ri";
+import { RiLockPasswordFill, RiEyeCloseFill, RiEyeFill } from "react-icons/ri";
+import { useState } from "react";
 
 const Register = () => {
+  const [openEye, setOpenEye] = useState(false);
+  const handleOpenEye = () => {
+    setOpenEye(!openEye);
+  };
+
+  const handleSbmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const username = form.get("username");
+    const email = form.get("email");
+    const photoURL = form.get("photo");
+    const password = form.get("password");
+    console.log(username, email, photoURL, password);
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex flex-col">
         <h2 className="text-4xl">Register Now</h2>
         <div className="shadow-2xl bg-base-100 rounded-2xl">
-          <form className="p-5 md:w-96 space-y-5">
+          <form className="p-5 w-64 md:w-96 space-y-5" onSubmit={handleSbmit}>
             <div className="form-control">
               <label className="input input-bordered flex items-center gap-2">
                 <FaUser />
-                <input type="text" className="grow" placeholder="Username" />
+                <input
+                  type="text"
+                  name="username"
+                  className="grow"
+                  placeholder="Username"
+                  required
+                />
               </label>
             </div>
             <div className="form-control">
               <label className="input input-bordered flex items-center gap-2">
                 <MdEmail />
-                <input type="email" className="grow" placeholder="Email" />
+                <input
+                  type="email"
+                  name="email"
+                  className="grow"
+                  placeholder="Email"
+                  required
+                />
               </label>
             </div>
             <div className="form-control">
               <label className="input input-bordered flex items-center gap-2">
                 <FaLink />
-                <input type="text" className="grow" placeholder="Photo url" />
+                <input
+                  type="text"
+                  name="photo"
+                  className="grow"
+                  placeholder="Photo url"
+                  required
+                />
               </label>
             </div>
             <div className="form-control">
               <label className="input input-bordered flex items-center gap-2">
                 <RiLockPasswordFill />
-                <input type="text" className="grow" placeholder="Password" />
-                <button className="kbd kbd-sm">
-                  <RiEyeCloseFill />
-                </button>
+                <span onClick={handleOpenEye} className="sm:hidden">
+                  {openEye ? <RiEyeFill /> : <RiEyeCloseFill />}
+                </span>
+                <input
+                  type={openEye ? "text" : "password"}
+                  className="grow"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+                <span onClick={handleOpenEye} className="hidden sm:flex">
+                  {openEye ? <RiEyeFill /> : <RiEyeCloseFill />}
+                </span>
               </label>
               <label className="label">
                 <Link
@@ -46,7 +89,7 @@ const Register = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Register</button>
             </div>
           </form>
         </div>
